f9e49b81597b499328831ae27ddf77192f390070
daklo91 <coffeedog91@gmail.com>
2026-08-26 13:41:43 +0200

    Add form and filter function

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [placeType, setPlaceType] = useState("cafe");
+  const [maxTravelMinutes, setMaxTravelMinutes] = useState(30);
+  const [travelMethod, setTravelMethod] = useState("walking");
+  const [needsQuiet, setNeedsQuiet] = useState(false);
+  const [needsPower, setNeedsPower] = useState(false);
+
   type Place = {
     id: string;
     name: string;
@@ -30,11 +40,78 @@ export default function Home() {
     },
   ];
 
+  function onFilterPlaces(event: React.SubmitEvent) {
+    event.preventDefault();
+    console.log("placeType: " + placeType);
+    console.log("maxTravelMinutes: " + maxTravelMinutes);
+    console.log("needsQuiet: " + needsQuiet);
+    console.log("needsPower: " + needsPower);
+  }
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Welcome, Nomad!</h1>
+        <h1>Welcome!</h1>
         <p>Pick a place for today:</p>
+        <form onSubmit={onFilterPlaces}>
+          <label htmlFor="quietCheckbox">Quiet</label>
+          <input
+            defaultChecked={needsQuiet}
+            name="quietCheckbox"
+            id="quietCheckbox"
+            type="checkbox"
+            onChange={(e) => setNeedsQuiet(e.target.checked)}
+          />
+          <label htmlFor="powerCheckbox">Power</label>
+          <input
+            defaultChecked={needsPower}
+            name="powerCheckbox"
+            id="powerCheckbox"
+            type="checkbox"
+            onChange={(e) => setNeedsPower(e.target.checked)}
+          />
+          {/* TODO: Koble dette sammen med hva som er i typescript */}
+          <label htmlFor="typeDropdown">Choose type of place:</label>
+          <select
+            defaultValue={placeType}
+            name="typeDropdown"
+            id="typeDropdown"
+            onChange={(e) => setPlaceType(e.target.value)}
+          >
+            <option value="cafe">Cafe</option>
+            <option value="library">Library</option>
+            <option value="outside">Outside</option>
+            <option value="addNew">Add new ...</option>
+          </select>
+          <label htmlFor="travelMethodDropdown">Choose travel method:</label>
+          <select
+            defaultValue={travelMethod}
+            name="travelMethodDropdown"
+            id="travelMethodDropdown"
+            onChange={(e) => setTravelMethod(e.target.value)}
+          >
+            <option value="walking">Walking</option>
+            <option value="bus">Bus</option>
+            <option value="train">Train</option>
+            <option value="addNew">Add new ...</option>
+          </select>
+          <label htmlFor="maxTravelTimeInput">
+            Maximum travel time in minutes
+          </label>
+          <input
+            name="maxTravelTimeInput"
+            id="maxTravelTimeInput"
+            type="number"
+            defaultValue={maxTravelMinutes}
+            onKeyDown={(e) => {
+              if (["e", "E", "+", "-"].includes(e.key)) {
+                e.preventDefault();
+              }
+            }}
+            onChange={(e) => setMaxTravelMinutes(+e.target.value)}
+          />
+          <input type="submit" value="Find places" />
+        </form>
         <ul>
           {places.map((place) => (
             <li key={place.id}>
